0b1a2ccd221ba394de2ed1236eba2d2150c25cd9
miko <miko@mikodeMacBook-Pro-2.local>
2024-03-26 16:57:11 -0700

    init

--- app.py
+++ app.py
@@ -1,2 +1,38 @@
 import streamlit as st
-st.title("hi,I'm Jia Shu")
+st.title("👋I'm Jia Shu")
+st.header("I'm a UX Designer 👩‍🎨")
+st.image('./images/profilepic.png')
+
+st.header("About")
+st.markdown(
+    """Education: UW MSTI student
+    Work experience: worked as PM in Ovopark
+    Hobbies: Design and sleep...
+    Interesting Projects: Unknown
+    """
+)
+
+# col1, col2 = st.columns(2)
+# with col1:
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
