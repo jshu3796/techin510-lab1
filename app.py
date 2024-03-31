39869ac85693c2d1cdca647091c0ec143cb97b77
miko <miko@mikodeMacBook-Pro-2.local>
2024-03-31 13:53:47 -0700

    lab 1 3.31

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 import streamlit as st
 st.title("👋I'm Jia Shu")
+st.divider()
 st.header("I'm a UX Designer 👩‍🎨")
 st.image('images/profilepic.jpg')
 
 st.header("About")
-st.markdown(
-    """Education: UW MSTI student
-    Work experience: worked as PM in Ovopark
-    Hobbies: Design and sleep...
-    Interesting Projects: Unknown
-    """
-)
+st.divider()
+st.markdown('''
+            :red[Education] :black[UW-MSTI Student]''')
+st.markdown('''
+            :blue[Work Experience] :black[PM in Ovopark, UX designer in Rallyville]''')
+st.markdown('''
+            :rainbow[Hobbies and Interests] :black[Design 🧑‍🎨 , Hiking ⛰️, Coffee ☕️]''')
+st.markdown('''
+            :orange[Interesting Projects] :black[http://sheisjiashu.com]''')
+
+
+# st.markdown(
+#     """Education: UW MSTI student
+#     Work experience: worked as PM in Ovopark
+#     Hobbies: Design and sleep...
+#     Interesting Projects: Unknown
+#     """
+# )
 
 # col1, col2 = st.columns(2)
 # with col1:
